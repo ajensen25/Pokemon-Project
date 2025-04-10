06447c4a088f7b7c7d6b51a2d55ec126fab139c8
ajensen25 <aidenjensen707@gmail.com>
2025-04-10 17:51:10 -0400

    got to display information correctly

--- src/pages/Search/Search.jsx
+++ src/pages/Search/Search.jsx
@@ -1,4 +1,4 @@
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import "./Search.css";
 
 function Search() {
@@ -9,21 +9,30 @@ function Search() {
 
   const onInputPressed = (e) => {
     if (e.key === "Enter" && cardInput.current.value.trim()) {
-      search();
+      search(cardInput.current.value);
     }
   };
 
-  const search = async () => {
+  const search = async (input) => {
     try {
-      const URL = `https://pokeapi.co/api/v2/pokemon/${cardInput.current.value}`;
+      const URL = `https://pokeapi.co/api/v2/pokemon/${input}`;
 
       const res = await fetch(URL);
       const cardData = await res.json();
-      console.log(cardData);
+      setData({
+        name: cardData.name,
+        image: cardData.sprites.front_default,
+        stats: cardData.stats,
+        abilities: cardData.abilities,
+      });
     } catch (err) {
       console.error("Failed to fetch API data", err);
     }
   };
+
+  useEffect(() => {
+    search("Pikachu");
+  }, []);
 
   return (
     <div className="search">
@@ -37,7 +46,47 @@ function Search() {
         ref={cardInput}
         onKeyDown={onInputPressed}
       />
-      <p>JSON</p>
+      {data && (
+        <div className="data-container">
+          <img src={data.image} alt="" />
+          <h2>{data.name}</h2>
+          <div className="card-info">
+            <ul className="stats">
+              <li>
+                <b>HP:</b>
+                {data.stats[0].base_stat}
+              </li>
+              <li>
+                <b>Attack:</b>
+                {data.stats[1].base_stat}
+              </li>
+              <li>
+                <b>Defense:</b>
+                {data.stats[2].base_stat}
+              </li>
+              <li>
+                <b>Spec. Attack:</b>
+                {data.stats[3].base_stat}
+              </li>
+              <li>
+                <b>Spec. Defense:</b>
+                {data.stats[4].base_stat}
+              </li>
+              <li>
+                <b>Speed:</b>
+                {data.stats[5].base_stat}
+              </li>
+            </ul>
+            <ul className="abilities">
+              {data.abilities.map((ability, index) => (
+                <li key={index}>
+                  <b>Ability {index + 1}:</b> {ability.ability.name}
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
